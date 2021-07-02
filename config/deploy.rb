5d4bf2365bd6c8ca34239fa168e0c570a3a0eba5
WANG Yanfei <mintywangtvt@gmail.com>
2021-07-02 21:49:02 +0800

    Initial commitgit

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.14.1"
 
-set :application, "gutmeta_platform"
+set :application, "gapp_rails"
 set :repo_url, "git@delta.cs.cityu.edu.hk:chelijia/meta_platform.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, "/home/platform/meta_platform"
+set :deploy_to, "/home/platform/gapp_rails"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
