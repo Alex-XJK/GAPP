72eb04af19d720b4144e160f8ff6a61629d27811
Xun Zhang <59300821+xz-cs@users.noreply.github.com>
2021-07-08 17:56:50 +0800

    Change http authenticate, develop "users" page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+    http_basic_authenticate_with name: "admin", password: "gapp"
     $user_stor_dir = "#{Rails.root}/data/user"
+
+    def index
+        @projects = App.order(:name)
+        @ana_cate = Analysis.order(:name)
+        @ac_attrs = Analysis.column_names
+        @viz = Task.order(:name)
+        @viz_attrs = Task.column_names
+        @ana = Analysis.order(:name)
+        @a_attrs = Analysis.column_names
+
+        @users = User.select(:id, :username, :created_at)
+        @usercolumn = ["id", "username", "created_at"] 
+    end
 
     def show
         id = session[:user_id]
