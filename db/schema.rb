14561ebae414b72cb27fcd273bf719b2b243c086
Xun Zhang <59300821+xz-cs@users.noreply.github.com>
2021-08-26 14:35:49 +0800

    Update Admin and Account systems

--- db/schema.rb
+++ db/schema.rb
@@ -97,7 +97,7 @@ ActiveRecord::Schema.define(version: 2021_08_23_040414) do
   end
 
   create_table "apps", force: :cascade do |t|
-    t.string "app_no"
+    t.string "app_no", null: false
     t.string "name", null: false
     t.integer "price", null: false
     t.text "description", null: false
@@ -105,22 +105,22 @@ ActiveRecord::Schema.define(version: 2021_08_23_040414) do
     t.string "status", default: "offline"
     t.bigint "user_id"
     t.bigint "analysis_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.bigint "category_id"
     t.string "cover_image"
     t.string "panel"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.index ["analysis_id"], name: "index_apps_on_analysis_id"
     t.index ["category_id"], name: "index_apps_on_category_id"
     t.index ["user_id"], name: "index_apps_on_user_id"
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "initial", null: false
-    t.integer "serial", default: 1
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "initial"
+    t.integer "serial"
   end
 
   create_table "roles", force: :cascade do |t|
@@ -131,20 +131,30 @@ ActiveRecord::Schema.define(version: 2021_08_23_040414) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.integer "app_id"
-    t.string "usedData", default: [], array: true
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
+    t.bigint "analysis_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "app_id", null: false
     t.string "task_id"
     t.string "status"
+    t.index ["analysis_id"], name: "index_tasks_on_analysis_id"
+    t.index ["app_id"], name: "index_tasks_on_app_id"
     t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
+
+  create_table "user_roles", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "role_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["role_id"], name: "index_user_roles_on_role_id"
+    t.index ["user_id"], name: "index_user_roles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
-    t.string "dataFiles", default: [], array: true
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "role_id"
@@ -154,7 +164,8 @@ ActiveRecord::Schema.define(version: 2021_08_23_040414) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "tasks", "users"
+  add_foreign_key "apps", "categories"
+  add_foreign_key "tasks", "apps"
   add_foreign_key "users", "accounts"
   add_foreign_key "users", "roles"
 end
